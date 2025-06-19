92419b37e39f27b564cadeb96facfca07c024f96
Hisuiseki <kozaki.m.1001@icloud.com>
2025-06-19 10:47:28 +0900

    エディット

---
+++ task_edit.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>編集画面</title>
+</head>
+
+<body>
+    <form action="index.php"></form>
+        <h1>タスク編集</h1>
+            内容：<input type="text" name="edname"><br>
+            期限：<input type="date" name="eddate"><br>
+            優先度：<input type="select" name="edprio"><br>
+            状態：<input type="select" name="edsta">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
