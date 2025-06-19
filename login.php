62e69ad5d55cf589bd22f69f7bca5a2254bdf926
yuki2004831 <ubei138sy@gmail.com>
2025-06-19 10:29:42 +0900

    waku

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login_session.php" method="post">
+    <h1>ログイン</h1>
+    ユーザー名:
+    <input type="text" name="user_name"><br>
+    パスワード:
+    <input type="password"><br>
+    <button type="submit">ログイン</button>
+    </form>
+        <a href="./register.php">新規登録</a>
+</body>
+</html>
