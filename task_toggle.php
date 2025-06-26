f37baedd13d844d17aca7c7bb6fbd2d94bd2af55
Soyama1127 <2301017@s.asojuku.ac.jp>
2025-06-26 10:11:14 +0900

    タスク切り替え機能

--- task_toggle.php
+++ task_toggle.php
@@ -1,50 +1,13 @@
 <?php
 $pdo = new PDO('mysql:host=mysql322.phy.lolipop.lan;dbname=LAA1553893-todo;', 'LAA1553893', 'Todopass');
-$priority = ["低","中","高"]; 
-if (isset($_POST['search'])) {
-    if ($_POST['day'] !== 'すべて' && $_POST['priority'] !== 'すべて') {
-        // 期限が選択されている かつ 優先度も選択されている
-        $sql = $pdo->prepare('select * from todos where task like ? and (due_date-current_date)<=? and priority=? and user_id=?');
-        $sql->execute(['%' . $_POST['kerword'] . '%', $_POST['date'], $_POST['priority'], $_SESSION['user']['id']]);
-    } else if ($_POST['day'] !== 'すべて' && $_POST['prioriry'] == 'すべて') {
-        // 期限だけ選択されている
-        $sql = $pdo->prepare('select * from todos where task like ? and (due_date-current_date)<=? and user_id=?');
-        $sql->execute(['%' . $_POST['kerword'] . '%', $_POST['date'], $_SESSION['user']['id']]);
-    } else if ($_POST['day'] == 'すべて' && $_POST['prioriry'] !== 'すべて') {
-        // 優先度だけ選択されている
-        $sql = $pdo->prepare('select * from todos where task like ? and priority=? and user_id=?');
-        $sql->execute(['%' . $_POST['kerword'] . '%', $_POST['priority'], $_SESSION['user']['id']]);
-    } else {
-        // キーワードだけ
-        $sql = $pdo->prepare('select * from todos where task like ? user_id=?');
-        $sql->execute(['%' . $_POST['kerword'] . '%', $_SESSION['user']['id']]);
-    }
-} else {
-    $sql = $pdo->prepare('select * from todos where user_id=?');
-    $sql->execute([$_SESSION['user']['id']]);
+$id = $_REQUEST['id'];
+$sql = $pdo->prepare('select * from todos where id=?');
+$sql->execute([$id]);
+foreach ($sql as $row) {
+    $status = $row['status'] == 'todo' ? 'done' :'todo';
 }
-foreach ($sql as $row) :?>
-<table>
-    <thead>
-        <tr>
-            <th>状態</th>
-            <th>タスク</th>
-            <th>期限</th>
-            <th>優先度</th>
-            <th>操作</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td><input type="checkbox"></td>
-            <td><?= $row['task'] ?></td>
-            <td><?= $row['due_date'] ?></td>
-            <td><?= $priority[$row['priority']] ?></td>
-            <td>
-                <a href="task_edit.php?id=<?= $row['id'] ?>">編集</a>
-                <a onclick="confirm('削除しますか')" href="task_delete.php?id=<?= $row['id'] ?>">削除</a>
-            </td>
-        </tr>
-    </tbody>
-</table>
-<?php endforeach; ?>
+$sql = $pdo->prepare('update todos status=? where id=?');
+$sql->execute([$status,$id]);
+
+header("Location: index.php");
+?>
