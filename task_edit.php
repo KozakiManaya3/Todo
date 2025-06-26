1df1897ac94456412addc6822682a999da6579e2
Hisuiseki <kozaki.m.1001@icloud.com>
2025-06-26 11:09:07 +0900

    王手

--- task_edit.php
+++ task_edit.php
@@ -20,28 +20,21 @@
         $sql = $pdo->prepare('SELECT * FROM todos WHERE id = ?');
         $sql->execute([$id]);
 
-        foreach ($sql as $row){
-            $row['id'];
-            $row['user_id'];
-            $row['task'];
-            $row['status'];
-            $row['due_date'];
-            $row['priority'];
-    
-    ?>
+        foreach ($sql as $row) : ?>
+
     <form action="db.php" method="post">
         <h1>タスク編集</h1>
             内容：<input type="text" name="edname" value="<?= $row['task'] ?>"><br>
             期限：<input type="date" name="eddate" value="<?= $row['due_date'] ?>"><br>
             優先度：
-                <select name="edpr">
+                <select name="edprio">
                     <option value="0" <?= $row['priority'] == 0 ? 'selected' : '' ?> >低</option>
                     <option value="1" <?= $row['priority'] == 1 ? 'selected' : '' ?> >中</option>
                     <option value="2" <?= $row['priority'] == 2 ? 'selected' : '' ?> >高</option>
                 </select>
             <br>
             状態：
-                <select name="edst" >
+                <select name="edsta" >
                     <option value="todo" <?= $row['status'] === "todo" ? 'selected' : '' ?> >未完了</option>
                     <option value="done" <?= $row['status'] === "done" ? 'selected' : '' ?> >完了</option>
                 </select>
@@ -49,7 +42,7 @@
 
             <input type="hidden" name="id" value="<?php $row["id"] ?>">
         
-        <?php } ?>
+        <?php endforeach ; ?>
         <input type="submit" name="edit" value="保存">
         <a href="index.php">キャンセル</a>
     </form>
