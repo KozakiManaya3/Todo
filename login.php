6b203590999468ffef3cdcd763a199ab38080642
Soyama1127 <2301017@s.asojuku.ac.jp>
2025-07-03 10:05:13 +0900

    ログイン＆新規登録css

--- login.php
+++ login.php
@@ -9,16 +9,18 @@
 </head>
 
 <body>
-    <form action="db.php" method="post">
-        <h1>ログイン</h1>
-        ユーザー名:
-        <input type="text" name="user_name" value="Kozaki"><br>
-        パスワード:
-        <input type="password" name="password" value="aaa"><br>
-        <button type="submit" name="login">ログイン</button>
-    </form>
-    <br>
-    <a href="./register.php">新規登録</a>
+    <div class="login-register">
+        <form action="db.php" method="post">
+            <h1>ログイン</h1>
+            <p>ユーザー名</p>
+            <input type="text" name="user_name" value="Kozaki" class="login-register-input">
+            <p>パスワード</p>
+            <input type="password" name="password" value="aaa" class="login-register-input">
+            <button type="submit" name="login">ログイン</button>
+        </form>
+        <br>
+        <a href="./register.php">新規登録</a>
+    </div>
 </body>
 
 </html>
--- register.php
+++ register.php
@@ -1,18 +1,26 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>登録</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>ユーザー登録</h1>
-    <form action="db.php" method="post">
-        ユーザー名: <input type="text" name="username"><br>
-        パスワード: <input type="password" name="password"><br>
-        <button type="submit" name="register">登録</button><br>
-    </form>
-    <br>
-    <a href="./login.php">ログインはこちら</a>
+    <div class="login-register">
+        <h1>ユーザー登録</h1>
+        <form action="db.php" method="post">
+            <p>ユーザー名</p>
+            <input type="text" name="username" class="login-register-input"><br>
+            <p>パスワード</p>
+            <input type="password" name="password" class="login-register-input"><br>
+            <button type="submit" name="register">登録</button><br>
+        </form>
+        <br>
+        <a href="./login.php">ログインはこちら</a>
+    </div>
 </body>
+
 </html>
